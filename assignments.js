c571c4daee68737e1350cda7299ea5654484f98d
junonaostache <116108982+junonaostache@users.noreply.github.com>
2022-10-18 19:14:06 +0100

    Add files via upload

---
+++ assignments.js
@@ -0,0 +1,85 @@
+/*let country = "United Kingdom";
+let continent = "Europe";
+let population = 66000000;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIsland = "Isle of Man";
+isIsland = true;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+*/
+
+/*const markWeight1 = 78;
+const markHeight1 = 1.69;
+const johnWeight1 = 92;
+const johnHeight1 = 1.95;
+const markBMI1 = markWeight1 / markHeight1 ** 2;
+const johnBMI1 = johnWeight1 / johnHeight1 ** 2;
+if (markBMI1 > johnBMI1) {
+  console.log(`Mark's BMI (${markBMI1}) is higher than John's (${johnBMI1})!`);
+} else {
+  console.log(`John's BMI (${johnbMI1}) is higher than Mark's (${markBMI1})`);
+}
+
+const markWeight2 = 95;
+const markHeight2 = 1.88;
+const johnWeight2 = 85;
+const johnHeight2 = 1.76;
+const markBMI2 = markWeight2 / markHeight2 ** 2;
+const johnBMI2 = johnWeight2 / johnHeight2 ** 2;
+console.log(markBMI2, johnBMI2);
+if (markBMI2 > johnBMI2) {
+  console.log(
+    `Mark's BMI (${markBMI2}) is now higher than John's (${johnBMI2})!`
+  );
+} else {
+  console.log(
+    `Mark's BMI (${markBMI2}) is now lower than John's (${johnBMI2})!`
+  );
+}
+*/
+
+//CHALLENGE 3
+
+// let Dolphins = (96 + 108 + 89) / 3;
+// let Koalas = (88 + 91 + 110) / 3;
+
+// console.log(Dolphins, Koalas);
+// if ((Dolphins > 100 || Koalas > 100) && Dolphins > Koalas) {
+//   console.log("Dolphins are the winners!");
+// } else if ((Dolphins > 100 || Koalas > 100) && Dolphins < Koalas) {
+//   console.log("Koalas are the winners!");
+// } else if (Dolphins >= 100 && Koalas >= 100 && Dolphins === Koalas) {
+//   console.log("It's a draw!");
+// }
+// Dolphins = (97 + 112 + 101) / 3;
+// Koalas = (109 + 95 + 123) / 3;
+
+// console.log(Dolphins, Koalas);
+// if ((Dolphins > 100 || Koalas > 100) && Dolphins > Koalas) {
+//   console.log("Dolphins are the winners!");
+// } else if ((Dolphins > 100 || Koalas > 100) && Dolphins < Koalas) {
+//   console.log("Koalas are the winners!");
+// } else if (Dolphins >= 100 && Koalas >= 100 && Dolphins === Koalas) {
+//   console.log("It's a draw!");
+// } else if (Dolphins < 100 && Koalas < 100) {
+//   console.log("No one gets the trophy :(");
+// }
+// Dolphins = (97 + 112 + 101) / 3;
+// Koalas = (109 + 95 + 106) / 3;
+
+// console.log(Dolphins, Koalas);
+// if ((Dolphins > 100 || Koalas > 100) && Dolphins > Koalas) {
+//   console.log("Dolphins are the winners!");
+// } else if ((Dolphins > 100 || Koalas > 100) && Dolphins < Koalas) {
+//   console.log("Koalas are the winners!");
+// } else if (Dolphins >= 100 && Koalas >= 100 && Dolphins === Koalas) {
+//   console.log("It's a draw!");
+// } else if (Dolphins < 100 && Koalas < 100) {
+//   console.log("No one gets the trophy :(");
+// }
---
+++ script.js
@@ -0,0 +1,242 @@
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let jonas_matilda = "JM";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+console.log(myFirstJob);
+
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+//console.log(typeof 23);
+//console.log(typeof "Jonas");
+
+javaScriptIsFun = "YES!";
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = "programmer";
+job = "teacher";
+
+lastName = "Juno";
+console.log(lastName);
+*/
+// Math operators
+/*const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2**3 means 2 to the power of 3 = 2 * 2 * 2;
+
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + " " + lastName);
+
+// Assignment operators
+let x = 10 + 5; //15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x +1
+x--;
+x--;
+console.log(x);
+
+//Comparison operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);*/
+
+/*const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);*/
+/*
+const markWeight1 = 78;
+const markHeight1 = 1.69;
+const johnWeight1 = 92;
+const johnHeight1 = 1.95;
+const markBMI1 = markWeight1 / markHeight1 ** 2;
+const johnBMI1 = johnWeight1 / johnHeight1 ** 2;
+console.log(markBMI1, johnBMI1);
+const markHigherBMI = markBMI1 > johnBMI1;
+console.log(markHigherBMI);
+
+const markWeight2 = 95;
+const markHeight2 = 1.88;
+const johnWeight2 = 85;
+const johnHeight2 = 1.76;
+const markBMI2 = markWeight2 / markHeight2 ** 2;
+const johnBMI2 = johnWeight2 / johnHeight2 ** 2;
+console.log(markBMI2, johnBMI2);
+const markHigherBMI2 = markBMI2 > johnBMI2;
+console.log(markHigherBMI2);
+*/
+/*const firstName = "Jonas";
+const job = "teacher";
+const birthYear = 1991;
+const year = 2037;
+
+const jonas =
+  "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(jonasNew);
+
+console.log(`Just a regular string...`);
+console.log("String with \n\
+multiple \n\
+lines");
+
+console.log(`String
+multiple
+lines`);
+*/
+
+// IF/ELSE
+/*
+const age = 15;
+if (age >= 18) {
+  console.log("Sarah can start driving licence");
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearsLeft} years`);
+}
+
+const birthYear = 2012;
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century);
+*/
+
+/*
+// type conversion
+const inputYear = "1991";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number("Jonas"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion
+console.log("I am " + 23 + " years old");
+console.log("23" - "10" - 3);
+console.log("23" / "2");
+*/
+
+//5 falsy values: 0, ' ', undefined, null, NaN
+/*
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Jonas"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+const money = 100;
+if (money) {
+  console.log("Don't spend it all");
+} else {
+  console.log("You should get a job bitch");
+}
+
+let height = 0;
+if (height) {
+  console.log("YAY! Height is defined");
+} else {
+  console.log("Height is UNDEFINED");
+}
+*/
+
+/*
+// Equality operators === and differing !==
+const age = "18";
+if (age === 18) console.log("You just became an adult (strict)");
+if (age == 18) console.log("You just became an adult (loose)");
+
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) {
+  console.log("Cool! 23 is an amazing number");
+} else if (favourite === 7) {
+  console.log("7 is also a cool number!");
+} else if (favourite === 9) {
+  console.log("9 is also a cool number)");
+} else {
+  console.log("Number is not 23 or 7 or 9");
+}
+
+if (favourite !== 23) console.log("Why not 23?");
+*/
+
+// const hasDriversLicence = true; //A
+// const hasGoodVision = true; //B
+// console.log(hasDriversLicence && hasGoodVision); //and
+// console.log(hasDriversLicence || hasGoodVision); //or
+// console.log(!hasDriversLicence); //not
+
+// // if (hasDriversLicence && hasGoodVision) {
+// //   console.log("Sarah is able to drive");
+// // } else {
+// //   console.log("Someone else should drive");
+// // }
+
+// const isTired = false; //C
+// console.log(hasDriversLicence && hasGoodVision && isTired); //or
+
+// if (hasDriversLicence && hasGoodVision && !isTired) {
+//   console.log("Sarah is able to drive");
+// } else {
+//   console.log("Someone else should drive");
+// }
+
+//THE SWITCH STATEMENT
